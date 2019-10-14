c6627e52c8f51b4cdda8f01b2268f4ae95c9e1d5
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-10-13 22:29:04 -0500

    Adds theme and global styles injection

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,10 +9,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi and welcome.</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
