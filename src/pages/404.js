43efdfc2fdcfb60a804d58d56e81f5fb142005e1
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-10-13 22:48:30 -0500

    Updates file structure

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
 
 const NotFoundPage = () => (
   <Layout>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
 
 const SecondPage = () => (
   <Layout>
