27194bba7e06ecca10b988276001709c70660a09
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-10-16 23:30:20 -0500

    Gets ready for self hosted fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,21 @@ import Header from "../components/sections/header"
 import Features from "../components/sections/features"
 import Footer from "../components/sections/footer"
 
+// Notes for fonts -
+// step 1. add WOFF fonts to static/fonts
+//step 2. add font face styles to
+// https://transfonter.org/
+// @font-face {
+//   font-family: 'MyWebFont';
+//   src:  url('myfont.woff2') format('woff2'),
+//         url('myfont.woff') format('woff');
+// }
+// @font-face {
+//   font-family: "Lato";
+//   src: url("../../static/fonts/lato/Lato-Regular.woff")   format("woff");
+// }
+// https://medium.com/@sergeybulavyk/mastering-your-fonts-in-gatsby-fbdf2782e083
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,6 +3,16 @@ import { createGlobalStyle } from "styled-components"
 const normalize = `
   /*! modern-normalize | MIT License | https://github.com/sindresorhus/modern-normalize */html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}:root{-moz-tab-size:4;tab-size:4}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'}hr{height:0}abbr[title]{text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:SFMono-Regular,Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{padding:0}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
 `
+// https://transfonter.org/ -- use to gen WOFF fonts
+//
+// @font-face {
+//   font-family: "Lato";
+//   src: url("../../static/fonts/lato/Lato-Regular.woff")   format("woff");
+// }
+// @font-face {
+//   font-family: "Lato";
+//   src: url("../../static/fonts/lato/Lato-Regular.woff")   format("woff");
+// }
 
 const GlobalStyles = createGlobalStyle`
   ${normalize};
@@ -75,3 +85,103 @@ const GlobalStyles = createGlobalStyle`
 `
 
 export default GlobalStyles
+
+/*
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-LightItalic.woff2') format('woff2'),
+        url('HKGrotesk-LightItalic.woff') format('woff');
+    font-weight: 300;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Italic.woff2') format('woff2'),
+        url('HKGrotesk-Italic.woff') format('woff');
+    font-weight: normal;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-ExtraBold.woff2') format('woff2'),
+        url('HKGrotesk-ExtraBold.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Medium.woff2') format('woff2'),
+        url('HKGrotesk-Medium.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-SemiBoldItalic.woff2') format('woff2'),
+        url('HKGrotesk-SemiBoldItalic.woff') format('woff');
+    font-weight: 600;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Regular.woff2') format('woff2'),
+        url('HKGrotesk-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-BoldItalic.woff2') format('woff2'),
+        url('HKGrotesk-BoldItalic.woff') format('woff');
+    font-weight: bold;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Light.woff2') format('woff2'),
+        url('HKGrotesk-Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Black.woff2') format('woff2'),
+        url('HKGrotesk-Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-MediumItalic.woff2') format('woff2'),
+        url('HKGrotesk-MediumItalic.woff') format('woff');
+    font-weight: 500;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-Bold.woff2') format('woff2'),
+        url('HKGrotesk-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'HK Grotesk';
+    src: url('HKGrotesk-SemiBold.woff2') format('woff2'),
+        url('HKGrotesk-SemiBold.woff') format('woff');
+    font-weight: 600;
+    font-style: normal;
+}
+
+
+*/
