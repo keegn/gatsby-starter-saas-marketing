9062a216ab346aab1f8880c0f33e964749873f51
Adrian Bece <adrian@prototyp.digital>
2020-11-30 08:23:01 +0100

    Wrap page element

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "./src/components/common/layout/layout"
 
-export const wrapRootElement = ({ element }) => {
+export const wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>
 }
