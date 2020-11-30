c63711c483ae1c649bd2c6b2a6f28d1866fa8793
Adrian Bece <adrian@prototyp.digital>
2020-11-30 08:14:38 +0100

    Optimize theme loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from "react"
+import Layout from "./src/components/common/layout/layout"
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return <Layout>{element}</Layout>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
 import Navigation from "../components/common/navigation/navigation"
 
@@ -10,14 +9,14 @@ import Footer from "../components/sections/footer"
 import GetStarted from "../components/sections/getstarted"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <Navigation />
     <Header />
     <Features />
     <GetStarted />
     <Footer />
-  </Layout>
+  </>
 )
 
 export default IndexPage
