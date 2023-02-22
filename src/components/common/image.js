715e5ae467cdef7714d7665c7e59a7a75386045f
Josh <jcjohnson77@gmail.com>
2023-02-21 20:52:32 -0500

    Update packages and move gatsby-image to gatsby-plugin-image

--- src/components/common/image.js
+++ src/components/common/image.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -18,15 +18,13 @@ const Image = () => {
     query {
       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
+          gatsbyImageData(layout: CONSTRAINED)
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <GatsbyImage image={data.placeholderImage.childImageSharp.gatsbyImageData} />
 }
 
 export default Image
